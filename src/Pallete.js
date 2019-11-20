c9b75c2aad75fcb07c3d1c5738e9eda1340a3ace
RubinoPD <robertaskask@gmail.com>
2019-11-20 22:57:53 +0200

    Add color style select

--- src/Pallete.js
+++ src/Pallete.js
@@ -6,19 +6,23 @@ import './Pallete.css';
 export default class Pallete extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { level: 500 };
+		this.state = { level: 500, format: 'hex' };
 		this.changeLevel = this.changeLevel.bind(this);
+		this.changeFormat = this.changeFormat.bind(this);
 	}
 	changeLevel(level) {
 		this.setState({ level });
 	}
+	changeFormat(val) {
+		this.setState({ format: val });
+	}
 	render() {
 		const { colors } = this.props.palette;
-		const { level } = this.state;
-		const colorBoxes = colors[level].map((color) => <ColorBox background={color.hex} name={color.name} />);
+		const { level, format } = this.state;
+		const colorBoxes = colors[level].map((color) => <ColorBox background={color[format]} name={color.name} />);
 		return (
 			<div className="Pallete">
-				<Navbar level={level} changeLevel={this.changeLevel} />
+				<Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
 				<div className="Pallete-colors">{colorBoxes}</div>
 				{/* footer */}
 			</div>
